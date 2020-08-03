238b49fb869ec7cf3e420d1d768be4e3b9e5d508
TheGithubMafia <portfoliomafia@tuta.io>
2020-08-03 13:29:35 +0530

    refactor

---
+++ content/content-script.js
@@ -0,0 +1,41 @@
+console.log("Web dictionary successfully added!");
+
+const show_dictionary_tooltip = async (e) => {
+
+    let mouseX = e.pageX;
+    let mouseY = e.pageY;
+
+    let tooltip = new Dictionary_tooltip();
+    tooltip.create_tooltip();
+    tooltip.place_tooltip(mouseX, mouseY);
+
+    let word = window.getSelection().toString().trim();
+    if (word.length > 0) {
+        let definition = await retrieve_definition_via_api(word);
+        tooltip.write_definition(word, definition);
+    } else {
+        tooltip.write_definition(tool, word, "ERROR : Not found !");
+    }
+
+    // to reposition after filling the data
+    tooltip.place_tooltip(mouseX, mouseY);
+}
+
+const delete_all_dictionary_tooltip = (e) => {
+    let reg = new RegExp("dict");
+    let tar = e.target.classList.toString();
+    let arr = document.querySelectorAll('.dict');
+    if (!reg.test(tar)) {
+        for (let i of arr) {
+            i.remove();
+        }
+    }
+}
+
+document.ondblclick = (e) => {
+    show_dictionary_tooltip(e);
+};
+
+document.onclick = (e) => {
+    // delete_all_dictionary_tooltip(e);
+};
--- popup/scripts/main.js
+++ popup/scripts/main.js
@@ -1,52 +1,36 @@
-// entry point
-
 window.onload = () => {
-    // event listener for search submit button
+
     let search_submit_button = document.getElementById("submit");
     search_submit_button.onclick = () => {
-        get_data_and_write();
+        get_data_and_write_definition();
     };
 
-    // event listener for enter button
-    let search_field = document.getElementById("inp");
-    search_field.onkeydown = (e) => {
+    let search_input_field = document.getElementById("inp");
+    search_input_field.onkeydown = (e) => {
         let key = e.keyCode;
 
-        // if enter is pressed
+        // key = 13 for enter button
         if (key == 13) {
-            get_data_and_write();
+            get_data_and_write_definition();
         }
-    }
-
-    // when toggle is clicked
-    let toggle = document.getElementById("cb");
-    toggle.onclick = () => {
-        browser.storage.local.set({ is_search_allowed: toggle.checked });
     }
 }
 
-// write definition from api call and write it to dom
-const get_data_and_write = async () => {
+const get_data_and_write_definition = async () => {
 
-    // get word from input
-    inp = document.getElementById("inp");
-    word = inp.value.trim();
-
-    // show loading
-    write_definition_to_dom(word, "loading...");
-
-    // get definition from api
+    word = document.getElementById("inp").value.trim();
     let definition = await retrieve_definition_via_api(word);
-    // console.log(definition);
 
-    // if word is valid
+    let existing_dictionary_dom = document.getElementsByClassName("result")[0];
+
+    let tooltip = new Dictionary_tooltip();
+    tooltip.create_tooltip(existing_dictionary_dom);
+    tooltip.write_definition(word, "loading...");
+
     if (word.length > 0) {
-        // assign these elements now
-        write_definition_to_dom(word, definition);
+        tooltip.write_definition(word, definition);
     } else {
-        // if not show error
-        write_definition_to_dom(word, "ERROR : Not found !");
+        tooltip.write_definition(word, "ERROR : Not found !");
     }
-
 }
 
